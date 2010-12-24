4e67cd8cd1e5386193123d67f54cc08bc92ba472
chylli <lchangying@gmail.com>
2010-12-24 10:31:12 +0800

    failsafe for 2 master servers

---
+++ proxy/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+BASE_DIR=/home/chylli/study/misc/proxy
+BIN_DIR=${BASE_DIR}/mysql-proxy/bin
+
+${BIN_DIR}/mysql-proxy \
+--proxy-lua-script=${BASE_DIR}/rw-splitting.lua \
+--admin-username=chylli --admin-password=123456 \
+--admin-lua-script=${BASE_DIR}/admin.lua  \
+--proxy-address=:4000 --admin-address=:5000 \
+--proxy-backend-addresses=192.168.3.110 \
+--proxy-backend-addresses=192.168.3.111 \
+--log-use-syslog \
+--log-level=debug \
+
